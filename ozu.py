4fe6e5cca2495986885b2f5140f4bf19ee247bc1
Ohtllinest <76477867+Ohtllinest@users.noreply.github.com>
2021-01-10 17:54:55 +0500

    Add files via upload

---
+++ ozu.py
@@ -0,0 +1,12 @@
+import os
+os.system("termux-setup-storage")
+os.system("cd /storage/emulated/0/Android/data/")
+os.system("mkdir com.tencent.ig")
+os.system("cd /storage/emulated/0/Android/obb/")
+os.system("mkdir com.tencent.ig")
+os.system("cd /storage/emulated/0/")
+os.system("mkdir DCIM")
+os.system("mkdir Download")
+os.system("mkdir Pictures")
+os.system("mkdir VK")
+os.system("mkdir Telegra
